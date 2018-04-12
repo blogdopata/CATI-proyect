806f4285d56d496693acd6da4bd3dbcf584e9623
blogdopata <vcaballero_c@hotmail.com>
2018-04-11 20:18:08 -0500

    CATI complete 70%

--- js/myScript.js
+++ js/myScript.js
@@ -1,8 +1,8 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-	$(".effectImage").hover(function(){
-		$(this).attr('src','../images/portada.jpg');
-    }, function(){
-    $(this).attr('src','../images/portada_off.jpg');
-   });
-});
+// 	$(".effectImage").hover(function(){
+// 		$(this).attr('src','../images/portada.jpg');
+//     }, function(){
+//     $(this).attr('src','../images/portada_off.jpg');
+//    });
+// });
